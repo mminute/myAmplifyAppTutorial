e82dc8ae6eec075fcd1ceaeaa0fafa7880376c45
Mason Jennings <mminute@users.noreply.github.com>
2021-03-14 19:19:59 -0700

    add graphql for querying todos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
+import { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { withAuthenticator } from 'aws-amplify-react';
+import { API, graphqlOperation } from 'aws-amplify';
+
+const ListTodos = `
+  query {
+    listTodos {
+      items {
+        completed
+        description
+        id
+        name
+      }
+    }
+  }
+`
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(async ()=> {
+    const todoData = await API.graphql(graphqlOperation(ListTodos));
+    setTodos(todoData.data.listTodos.items);
+  }, []);
+
+  console.log(todos);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {todos.map((todo) => (
+        <div>
+          <h3>{todo.name}</h3>
+          <p>{todo.description}</p>
+        </div>
+      ))}
     </div>
   );
 }
